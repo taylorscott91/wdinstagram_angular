64c1052093f25d3dd2889d344314eaac99fb8de7
Taylor Scott <TaylorScott@Taylors-MBP.fios-router.home>
2017-03-08 20:54:58 -0500

    homework stuff

--- js/app.js
+++ js/app.js
@@ -1,5 +1,64 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+let hardcodedData = [
+  {
+  photo_url: "http://fillmurray.com/300/200",
+  author: "author1",
+  body: "body1"
+  }, {
+  photo_url: "http://fillmurray.com/300/200",
+  author: "author2",
+  body: "body2"
+  }
+]
+
+angular
+.module("wdinstagram", [
+  "ui.router"
+])
+.config([
+  "$stateProvider",
+  RouterFunction
+])
+.controller("EntriesIndexController", [
+  EntriesIndexControllerFunction
+])
+.controller("EntriesShowController", [
+  "$stateParams",
+  EntriesShowControllerFunction
+])
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("entriesIndex", {
+    url: "/entries",
+    templateUrl: "/entries/index.html",
+    controller: "EntriesIndexController",
+    controllerAs: "indexVm"
+  })
+  .state("entryShow", {
+    url: "entries/:id",
+    templateUrl: "/entries/show.html",
+    controller: "EntriesShowController",
+    controllerAs: "showVm"
+  })
+}
+
+function EntriesIndexControllerFunction(){
+  var indexVm = this
+  indexVm.entries = hardcodedData
+  indexVm.newEntry = {}
+
+  indexVm.create = function(){
+    hardcodedData.push(indexVm.newEntry)
+    indexVm.newEntry = {}
+  }
+}
+
+function EntriesShowControllerFunction($stateParams){
+  var showVm = this
+  showVm.entry = hardcodedData[$stateParams.id]
+  showVm.update = function(){
+    hardcodedData[$stateParams.id] = showVm.entry
+  }
+}
